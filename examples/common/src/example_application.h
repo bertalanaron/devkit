2e00d20a4bc29a155dcd2e10bcc1b880c482263f
Ayayron1234 <bertalanaron01@gmail.com>
2025-11-02 13:08:08 +0100

    reworked examples asset handling. data location changed from one common to separate for each example

---
+++ examples/common/src/example_application.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <devkit/common/utils.h>
+#include <mini/ini.h>
+
+class ExampleApplicationBase {
+public:
+	ExampleApplicationBase()
+	{
+		// Parse ini file
+		m_ini = [] {
+			std::string path = dk::common::executable_path().string();
+			path.replace(path.find(".exe"), 4, ".ini");
+			mINI::INIFile file(path);
+			mINI::INIStructure ini;
+			file.read(ini);
+			return ini;
+		}();
+	}
+
+private:
+	mINI::INIStructure m_ini;
+
+protected:
+	auto& ini() { return m_ini; }
+	const auto& ini() const { return m_ini; }
+
+	std::optional<std::string> ini(const std::string& label, const std::string& value)
+	{
+		if (!m_ini.has(label) || !m_ini[label].has(value))
+			return std::nullopt;
+		return m_ini[label][value];
+	}
+
+	std::string ini_or(
+		const std::string& label, 
+		const std::string& value, 
+		const std::string& fallback)
+	{
+		const auto result = ini(label, value);
+		if (result.has_value())
+			return result.value();
+		return fallback;
+	}
+};
